4ac7d0a2813cbc0dec08ae7918391168a02762af
Thu Dang <thudang.dtt@gmail.com>
2022-11-11 10:24:22 -0600

    fixed mobile menu

--- script.js
+++ script.js
@@ -4,16 +4,15 @@ const overlay = document.getElementById("overlay");
 const openIcon = document.getElementById("openIcon");
 
 function openNav() {
-  console.log("heyyyy");
   openIcon.style.display = "none";
   menu.style.display = "block";
-  //   overlay.style.display = "block";
+  overlay.style.display = "block";
   menuOpen = true;
 }
 
 function closeNav() {
   menu.style.display = "none";
-  //   overlay.style.display = "none";
+  overlay.style.display = "none";
   openIcon.style.display = "block";
   menuOpen = false;
 }
